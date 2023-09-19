249a4e529fd9c1b6a58f83765ac3b3fcb934044a
jmilljr24 <jmilljr24@gmail.com>
2023-09-19 16:31:53 -0400

    remove like from posts database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_174304) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_203126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
-    t.integer "like"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
