90f030d1f013917efe015324d910feb4345cbf72
jmilljr24 <jmilljr24@gmail.com>
2023-09-19 12:44:35 -0400

    Add devise user and post scaffold

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
+  theme: {
+    // ...
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    // ...
+  ],
+};
