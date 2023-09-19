c5661127c78795a8132a01297ab7dd151d4c7b84
jmilljr24 <jmilljr24@gmail.com>
2023-09-19 15:53:14 -0400

    Add oauth user to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+  # def self.from_omniauth(access_token)
+  #   data = access_token.info
+  #   user = User.where(email: data['email']).first
+
+  #   # Uncomment the section below if you want users to be created if they don't exist
+  #   user ||= User.create(email: data['email'],
+  #                        password: Devise.friendly_token[0, 20])
+  #   user
+  # end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
